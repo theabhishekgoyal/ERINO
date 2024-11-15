fbb162097a551eec6f462f1a48effe5915dffff0
Abhishek Goyal <agoyal0308@gmail.com>
2024-11-15 20:49:26 +0530

    add verce.json

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,5 @@ mongoose.connect(process.env.MONGO_URL)
 app.use("/contacts", contactsRoutes);
 
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
